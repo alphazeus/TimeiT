98386c7ef5b569054c307a9b5ca138ca7bf8cdf7
Ferdinand <ferdinand0022@gmail.com>
2026-04-11 21:47:46 +0300

    Add the buttons as a separate entity.

--- contentInterface.py
+++ contentInterface.py
@@ -10,17 +10,25 @@ def on_tab_change(sender, app_data, user_data):
     elif app_data == "tab_timer":
         tab_state = "timer_selected"
 
-# def createClockContent():
+def createClockContent():
+    dpg.add_text(datetime.now().strftime("%H:%M:%S"), tag="clock")
+    dpg.bind_item_font("clock", "font_clock")
+
+def createTimerContent():
+    dpg.add_text("00:00", tag="timer_display")
+    dpg.add_button(label="Start")
+
+def createFunctionContent():
+    dpg.add_button(label="Clock", tag="clock_button")
+    dpg.add_button(label="Timer", tag="timer_button")
 
 def createContent():
-    with dpg.window(label="TimeiT", tag="main_window", no_title_bar=True, width=dpg.get_viewport_client_width(), height=dpg.get_viewport_client_height()):
-        with dpg.tab_bar(callback = on_tab_change):
-            with dpg.tab(label="Clock", tag = "tab_clock"):
-                dpg.add_text(datetime.now().strftime("%H:%M:%S"), tag="clock")
-                dpg.bind_item_font("clock", "font_clock")
-            with dpg.tab(label="Timer", tag = "tab_timer"):
-                dpg.add_text("00:00", tag="timer_display")
-                dpg.add_button(label="Start")
+    with dpg.window(label="Function", tag="function_window", no_title_bar=True, width=dpg.get_viewport_client_width()/5, height=dpg.get_viewport_client_height()):
+        createFunctionContent()
+    with dpg.window(label="Clock", tag="clock_window", no_title_bar=True, pos= [dpg.get_viewport_client_width()/5,0], width=dpg.get_viewport_client_width(), height=dpg.get_viewport_client_height()):
+        createClockContent()
+    with dpg.window(label="Timer", tag="timer_window", no_title_bar=True, width=dpg.get_viewport_client_width(), height=dpg.get_viewport_client_height(), show=False):
+        createTimerContent()
 
 
 def renderloop():
